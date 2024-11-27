33fc0a19e34f9c9fd62d93142f96a5d08f8d96b3
Thomas DuCourant <thomasducourant1@mail.weber.edu>
2024-11-27 15:46:28 -0700

    updated top

--- source_files/Top.vhd
+++ source_files/Top.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.extra_signals.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -42,7 +43,74 @@ end Top;
 
 architecture Behavioral of Top is
 
+    component MIDI_LOGIC is
+        Port ( CLK : in STD_LOGIC;
+               RST : in STD_LOGIC;
+               RX_RDY : in STD_LOGIC;
+               RX_PDATA : in STD_LOGIC_VECTOR (7 downto 0);
+               CHANNELS : out channels_array;
+               WAIT_COUNTS : out wait_counts_array;
+               VELOCITIES : out velocities_array;
+               PLAYER_ENS : out STD_LOGIC_VECTOR (PLAYER_COUNT - 1 downto 0));
+    end component;
+    
+    component UART_RX is
+    Port ( sdata_rx : in STD_LOGIC;
+           rst_rx : in STD_LOGIC;
+           clk_rx : in STD_LOGIC;
+           ready : out STD_LOGIC;
+           pdata_rx : out STD_LOGIC_VECTOR (7 downto 0));
+    end component;
+    
+    component Summer is
+    Port ( PL_array : in player_array;
+           PL_ACT : in STD_LOGIC_VECTOR (7 downto 0);
+           CLK : in STD_LOGIC;
+           SUM : out STD_LOGIC_VECTOR (11 downto 0));
+    end component;
+    
+    component Transmitter is
+    Port ( SUM : in STD_LOGIC_VECTOR (11 downto 0);
+           CLK : in STD_LOGIC;
+           SCK : out STD_LOGIC;
+           BUSY : out STD_LOGIC;
+           S_OUT : out STD_LOGIC);
+    end component;
+    
+    signal SUM_ntrl : STD_LOGIC_VECTOR (11 downto 0);
+    signal RDY_ntrl : STD_LOGIC;
+    signal PDATA_ntrl : STD_LOGIC_VECTOR (7 downto 0);
+
 begin
 
+    SUMMER_INST : Summer port map (
+        SUM => SUM_ntrl,
+        CLK => CLK,
+        PL_ACT => ,
+        PL_array => );
+        
+    TRANSMITTER_INST : Transmitter port map (
+        SUM => SUM_ntrl,
+        CLK => CLK,
+        SCK => SCK,
+        BUSY => SYNC,
+        S_OUT => S_OUT);
+        
+    MIDI_RX_INST : UART_RX port map (
+        clk_rx => CLK,
+        rst_rx => RST,
+        sdata_rx => S_IN,
+        ready => RDY_ntrl,
+        pdata_rx => PDATA_ntrl);
+        
+    MIDI_LOGIC_INST : MIDI_LOGIC port map (
+        CLK => CLK,
+        RST => RST,
+        RX_RDY => RDY_ntrl,
+        RX_PDATA => PDATA_ntrl,
+        CHANNELS => ,
+        WAIT_COUNTS => ,
+        VELOCITIES => ,
+        PLAYER_ENS => );
 
 end Behavioral;
